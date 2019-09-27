9e0a3e96e14a32f83bb7fce57f01024fd47eb962
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2019-09-27 16:45:26 +0900

    update download

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,10 @@ FROM node:current-alpine
 ENV REDPEN_VERSION 1.10.1
 
 RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ v0.9.13
-RUN wget -O - -q https://github.com/redpen-cc/redpen/releases/download/redpen-${REDPEN_VERSION}/redpen-${REDPEN_VERSION}.tar.gz && \
+RUN curl -sfLO https://github.com/redpen-cc/redpen/releases/download/redpen-${REDPEN_VERSION}/redpen-${REDPEN_VERSION}.tar.gz && \
     tar xvf redpen-${REDPEN_VERSION}.tar.gz && \
-    mv redpen-distribution-${REDPEN_VERSION}/bin bin
+    mv redpen-distribution-${REDPEN_VERSION} redpen && \
+    rm *.tar.gz
 RUN apk --update add git && \
     rm -rf /var/lib/apt/lists/* && \
     rm /var/cache/apk/*
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-if [ ! -f "bin/redpen" ]; then
+if [ ! -f "redpen/bin/redpen" ]; then
   npm install
 fi
 
-bin/redpen --version
+redpen/bin/redpen --version
 
 if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
-  bin/redpen -c config/redpen-conf-ja.xml -L ja -l 9999 -r plain "${INPUT_REDPEN_FLAGS:-'.'}" 2>/dev/null \
+  redpen/bin/redpen -c config/redpen-conf-ja.xml -L ja -l 9999 -r plain "${INPUT_REDPEN_FLAGS:-'.'}" 2>/dev/null \
     | reviewdog  -efm="%Z%f:%l: %m" -name="redpen" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}"
 else
   # github-pr-check (GitHub Check API) doesn't support markdown annotation.
-  bin/redpen -c config/redpen-conf-ja.xml -L ja -l 9999 -r plain "${INPUT_REDPEN_FLAGS:-'.'}" 2>/dev/null \
+  redpen/bin/redpen -c config/redpen-conf-ja.xml -L ja -l 9999 -r plain "${INPUT_REDPEN_FLAGS:-'.'}" 2>/dev/null \
     | reviewdog  -efm="%Z%f:%l: %m" -name="redpen" -diff="git diff HEAD^" -reporter=github-pr-check -level="${INPUT_LEVEL}"
 fi
