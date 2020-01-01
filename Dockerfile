10fccedc852c9c0b23233c1ed9a8cd4ab5b6cdae
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-01-01 14:56:30 +0900

    update 0.9.14

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV REDPEN_VERSION 1.10.3
 RUN apk --update add git curl && \
     rm -rf /var/lib/apt/lists/* && \
     rm /var/cache/apk/*
-RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ v0.9.13
+RUN wget -O - -q https://raw.githubusercontent.com/reviewdog/reviewdog/master/install.sh| sh -s -- -b /usr/local/bin/ v0.9.14
 RUN curl -sfLO https://github.com/redpen-cc/redpen/releases/download/redpen-${REDPEN_VERSION}/redpen-${REDPEN_VERSION}.tar.gz && \
     tar xvfp redpen-${REDPEN_VERSION}.tar.gz -C / && \
     rm *.tar.gz
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,9 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 redpen --version
 
-if [ "${INPUT_REPORTER}" == 'github-pr-review' ]; then
-  find "${INPUT_REDPEN_FLAGS:-'.'}" -type f -print0 \
-    | xargs -I{} -0 redpen -c config/redpen-conf-ja.xml -L ja -l 9999 -r plain {} 2>/dev/null \
-    | reviewdog  -efm="%f:%l: %m" -name="redpen" -diff="git diff HEAD^" -reporter=github-pr-review -level="${INPUT_LEVEL}"
-else
-  # github-pr-check (GitHub Check API) doesn't support markdown annotation.
-  find "${INPUT_REDPEN_FLAGS:-'.'}" -type f -print0 \
-    | xargs -I{} -0 redpen -c config/redpen-conf-ja.xml -L ja -l 9999 -r plain {} 2>/dev/null \
-    | reviewdog  -efm="%f:%l: %m" -name="redpen" -diff="git diff HEAD^" -reporter=github-pr-check -level="${INPUT_LEVEL}"
-fi
+find "${INPUT_REDPEN_FLAGS:-'.'}" -type f -print0                    \
+  | xargs -I{} -0 redpen -c config/redpen-conf-ja.xml -L ja -l 9999  \
+                         -r plain {} 2>/dev/null                     \
+  | reviewdog -efm="%f:%l: %m" -name="redpen" -diff="git diff HEAD^" \
+              -reporter="${INPUT_REPORTER:-'github-pr-check'}"       \
+              -level="${INPUT_LEVEL:-'error'}"
