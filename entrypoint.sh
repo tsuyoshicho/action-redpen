c13e73f199e754b3d1b6161b8f0e3756511cd0de
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-01-04 20:32:31 +0900

    entry small fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 redpen --version
 
 find "${INPUT_BASEDIR:-'.'}" -type f -name "${INPUT_TARGETS:-'*'}" -print0 \
-  | xargs -I{} -0 redpen -c config/redpen-conf-ja.xml -L ja -l 9999        \
-                         -r plain {} 2>/dev/null                           \
+  | xargs -I {} -0 redpen -c config/redpen-conf-ja.xml -L ja -l 9999       \
+                          -r plain {} 2>/dev/null                          \
   | reviewdog -efm="%f:%l: %m" -name="redpen" -diff="git diff HEAD^"       \
               -reporter="${INPUT_REPORTER:-'github-pr-check'}"             \
               -level="${INPUT_LEVEL:-'error'}"
