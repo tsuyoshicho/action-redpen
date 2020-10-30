e217ac8fa1328c1e2a6d826ed82534c503e5166b
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2020-10-31 07:54:07 +0900

    Update efm and style

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ redpen --version
 find "${INPUT_BASEDIR:-'.'}" -type f -name "${INPUT_TARGETS:-'*'}" -print0  \
   | xargs -I {} -0 redpen -c "${INPUT_CONFIG:-'config/redpen-conf-en.xml'}" \
                           -l 9999 -r plain {} 2>/dev/null                   \
-  | reviewdog -efm="%f:%l: %m" -name="redpen"                               \
+  | reviewdog -efm="%f:%l: Validation%t%*[a-z]%m at line:%.\*"              \
+              -name="redpen"                                                \
               -reporter="${INPUT_REPORTER:-'github-pr-check'}"              \
               -level="${INPUT_LEVEL:-'error'}"
